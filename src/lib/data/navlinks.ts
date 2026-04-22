c870ed38d4870312d85e4de77ba6c64a90aa5940
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2026-04-22 17:40:25 -0600

    feat(data): add nav links with section ids

---
+++ src/lib/data/navlinks.ts
@@ -0,0 +1,30 @@
+export const navLinks = [
+    {
+        label: "Home",
+        id: "hero",
+    },
+    {
+        label: "About",
+        id: "about",
+    },
+    {
+        label: "Experience",
+        id: "experience",
+    },
+    {
+        label: "Projects",
+        id: "projects",
+    },
+    {
+        label: "Stack",
+        id: "stack",
+    },
+    {
+        label: "Education",
+        id: "education",
+    },
+    {
+        label: "Contact",
+        id: "contact",
+    },
+];
